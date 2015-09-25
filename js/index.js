5331f122254004ef6c1d7a2684cde4fecd06f689
“student511” <student511@users.noreply.github.com>
2015-09-24 20:26:30 -0400

    changed js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,20 @@
-$(document).ready(function() { 
-  
-  var quoteList = ['"Yesterday is history; tomorrow is a mystery. Today is a gift, which is why we call it the present." - Bil Keane','"Once a year, go some place you\'ve never been before" - Unknown','"Create something today even if it sucks" - Unknown'];
-  
-  var quoteNumber = 0;
-  
-  $('.Quotes').html(quoteList[quoteNumber]);
-    quoteNumber++
-  
-  $('.nextQuote').on('click', function(){
-    
-    $('.Quotes').html(quoteList[quoteNumber]);
-    quoteNumber++
-    
-    if (quoteNumber === quoteList.length) {
-      quoteNumber = 0
-    }
+$(document).ready(function() {
+
+  var quoteList = ['"Yesterday is history; tomorrow is a mystery. Today is a gift, which is why we call it the present." - Bil Keane','"Once a year, go some place you\'ve never been before" - Unknown','"Create something today even if it sucks" - Unknown','“You are never too old to set another goal or to dream a new dream.” - C. S. Lewis'];
+
+   var quoteNumber = Math.floor(Math.random() * quoteList.length);
+
+            $('.Quotes').html(quoteList[quoteNumber]);
+
+   $('.nextQuote').on('click', function(){
+
+      $('.Quotes').html(quoteList[quoteNumber]);
+
+      quoteNumber = Math.floor(Math.random() * quoteList.length);
+
   });
-  
-  
-  
-  
+});
+
+
+
 });
